d495a264702c39e02734d26ef0c3a948d296684d
erica faulkenberry <erica.faulkenberry@dbtlabs.com>
2024-10-22 16:16:50 +0000

    unit tests

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,7 +1,28 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
+with
 
-from {{ source('jaffle_shop','orders') }}
+source as (
+
+    select * from {{ source('jaffle_shop',  'orders') }}
+
+),
+
+staged as (
+
+    select 
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        datediff('day', order_date, {{ dbt.current_timestamp() }} ) as days_since_ordered,
+        status like '%pending%' as is_status_pending,
+        case 
+            when status like '%shipped%' then 'shipped'
+            when status like '%return%' then 'returned'
+            when status like '%pending%' then 'placed'
+            when status like '%cancelled%' then 'cancelled'
+            else status
+        end as status
+    from source
+
+)
+
+select * from staged
