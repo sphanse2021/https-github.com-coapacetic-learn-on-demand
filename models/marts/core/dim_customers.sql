9d6f7c6b3e105c190c1f8383c580f63bcf38f268
erica faulkenberry <erica.faulkenberry@dbtlabs.com>
2024-11-19 22:44:10 +0000

    linting

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,9 +1,11 @@
 with customers as (
     select * from {{ ref('stg_customers')}}
 ),
+
 orders as (
     select * from {{ ref('fct_orders')}}
 ),
+
 customer_orders as (
     select
         customer_id,
@@ -14,6 +16,7 @@ customer_orders as (
     from orders
     group by 1
 ),
+
 final as (
     select
         customers.customer_id,
@@ -26,4 +29,5 @@ final as (
     from customers
     left join customer_orders using (customer_id)
 )
+
 select * from final
