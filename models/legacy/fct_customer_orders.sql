f78d874d66e3d4f0efc2873581de71052f9a894f
erica renée faulkenberry <efaulkenberry@gmail.com>
2025-02-01 20:17:49 -0600

    refactor

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -1,4 +1,6 @@
-with
+with 
+
+-- Import CTEs
 
 customers as (
 
@@ -18,57 +20,77 @@ payments as (
 
 ),
 
+-- Logical CTEs
+
+completed_payments as (
+    select 
+        orderid as order_id,
+        max(created) as payment_finalized_date,
+        sum(amount) / 100.0 as total_amount_paid
+    from payments
+    where status <> 'fail'
+    group by 1
+),
+
 paid_orders as (
-    select orders.id as order_id,
+    select 
+        orders.id as order_id,
         orders.user_id as customer_id,
         orders.order_date as order_placed_at,
         orders.status as order_status,
-        p.total_amount_paid,
-        p.payment_finalized_date,
-        c.first_name as customer_first_name,
-        c.last_name as customer_last_name
+        completed_payments.total_amount_paid,
+        completed_payments.payment_finalized_date,
+        customers.first_name as customer_first_name,
+        customers.last_name as customer_last_name
     from orders
-    left join (
-        select 
-            orderid as order_id,
-            max(created) as payment_finalized_date,
-            sum(amount) / 100.0 as total_amount_paid
-        from payments
-        where status <> 'fail'
-        group by 1
-    ) p on orders.id = p.order_id
-    left join customers as c on orders.user_id = c.id ),
+    left join completed_payments on orders.id = completed_payments.order_id
+    left join customers on orders.user_id = customers.id
+),
 
-customer_orders as (
-    select 
-        c.id as customer_id
-        , min(order_date) as first_order_date
-        , max(order_date) as most_recent_order_date
-        , count(orders.id) as number_of_orders
-    from customers as c 
-    left join orders on orders.user_id = c.id 
-    group by 1
+-- Final CTE
+
+final as (
+    select
+        paid_orders.order_id,
+        paid_orders.customer_id,
+        paid_orders.order_placed_at,
+        paid_orders.order_status,
+        paid_orders.total_amount_paid,
+        paid_orders.payment_finalized_date,
+        paid_orders.customer_first_name,
+        paid_orders.customer_last_name,
+
+        -- sales transaction sequence
+        row_number() over (order by paid_orders.order_id) as transaction_seq,
+
+        -- customer sales sequence
+        row_number() over (partition by paid_orders.customer_id order by paid_orders.order_id) as customer_sales_seq,
+        
+        -- new vs returning customer
+        case 
+            when (
+            rank() over (
+                partition by customer_id
+                order by order_placed_at, order_id
+                ) = 1
+            ) then 'new'
+        else 'return' end as nvsr,
+
+        -- customer lifetime value
+        sum(paid_orders.total_amount_paid) over (
+            partition by paid_orders.customer_id
+            order by paid_orders.order_placed_at
+            ) as customer_lifetime_value,
+
+        -- first day of sale
+        first_value(paid_orders.order_placed_at) over (
+            partition by paid_orders.customer_id
+            order by paid_orders.order_placed_at
+            ) as fdos
+    from paid_orders
 )
 
-select
-    p.*,
-    row_number() over (order by p.order_id) as transaction_seq,
-    row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
-    case when c.first_order_date = p.order_placed_at
-    then 'new'
-    else 'return' end as nvsr,
-    x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos
-from paid_orders p
-left join customer_orders as c using (customer_id)
-left outer join 
-(
-    select
-        p.order_id,
-        sum(t2.total_amount_paid) as clv_bad
-    from paid_orders p
-    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-    group by 1
-    order by p.order_id
-) x on x.order_id = p.order_id
+-- Simple Select Statment
+
+select * from final
 order by order_id
